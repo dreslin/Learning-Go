8a524471391e296551f5fd5930c97c0a39d38d74
dreslin <damiansjames@gmail.com>
2022-07-18 15:34:08 -0600

    headfirst go celcius

---
+++ tocelsius/main.go
@@ -0,0 +1,19 @@
+// converts temperature from Fahrenheit to Celsius.
+package main
+
+import (
+	"fmt"
+	"src/keyboard"
+)
+
+func main() {
+	fmt.Print("Enter a temperature in Fahrenheit: ")
+	fahrenheit, err := keyboard.GetFloat()
+	if err != nil {
+		panic(err)
+	}
+
+	celsius := (fahrenheit - 32) * 5 / 9
+	fmt.Printf("%0.2f degrees Celsius\n", celsius)
+
+}
